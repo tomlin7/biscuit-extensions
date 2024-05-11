d8be1bfb6c5f7d912fe94646bbd55420f8022152
Billy <billydevbusiness@gmail.com>
2024-05-11 11:29:48 +0530

    feat: Add clock, statusbar system stats extensions

---
+++ extensions/statsonbar.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import time
+
+__version__ = '0.0.1'
+__version_info__ = tuple([ int(num) for num in __version__.split('.')])
+
+import threading
+import typing
+
+if typing.TYPE_CHECKING:
+    from biscuit import ExtensionsAPI
+
+class Extension:
+    """System stats for Biscuit statusbar (author: @billyeatcookies)
+
+    Contributes:
+    - adds sysinfo to the statusbar
+    """
+
+    def __init__(self, api: ExtensionsAPI) -> None:
+        self.api = api
+        self.statusbar = self.api.statusbar
+        self.sysinfo = self.api.sysinfo
+        self.btn = self.statusbar.add_button(text=self.sysinfo.get_current_stats(), icon="pulse", description="CPU and Memory usage")
+        threading.Thread(target=self.update_stats, daemon=True).start()
+    
+    def update_stats(self) -> None:
+        while True:
+            time.sleep(1)
+            self.btn.change_text(self.sysinfo.get_current_stats())
+
+    def run(self) -> None:
+        ...
